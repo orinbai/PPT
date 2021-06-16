b3ff1a81e2c015b1432cea22eb1e087d7cdb987a
orinbai <orinbai@sina.com>
2021-06-16 22:04:52 +0800

    CSS过渡

--- CssHtml/app/static/js/main.js
+++ CssHtml/app/static/js/main.js
@@ -1,6 +1,8 @@
-var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]'
-var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]'
-var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]'
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var ESC_KEY = "Escape";
 
 function setDetails(imageUrl, titleText) {
     'use strict';
@@ -25,3 +27,49 @@ function setDetailsFromThumb(thumbnail) {
     'use strict';
     return setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 }
+
+function addThumbClickHandler (thumb) {
+    'use strict';
+    thumb.addEventListener('click', function (event) {
+        event.preventDefault();
+        showDetail();
+        setDetailsFromThumb(thumb);
+    })
+}
+
+function getThumbnailsArray() {
+    'use strict';
+    var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+    var thumbnailArray = [].slice.call(thumbnails);
+    return thumbnailArray;
+}
+
+function hideDetails() {
+    'use strict';
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetail () {
+    'use strict';
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+}
+
+function addKeyPressHandler() {
+    'use strict';
+    document.body.addEventListener('keyup', function (event) {
+        event.preventDefault();
+        console.log(event.key, event.code);
+        if (event.key === ESC_KEY) {
+            hideDetails();
+        }
+    })
+}
+
+function initializeEvents() {
+    'use strict';
+    var thumbnails = getThumbnailsArray();
+    thumbnails.forEach(addThumbClickHandler);
+    addKeyPressHandler();
+}
+
+initializeEvents();
