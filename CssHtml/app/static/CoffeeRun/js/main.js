3b94fd1461970b062a0ce537a326aa8ce6415721
orinbai <orinbai@sina.com>
2021-06-23 22:10:03 +0800

    bootstrap

--- CssHtml/app/static/CoffeeRun/js/main.js
+++ CssHtml/app/static/CoffeeRun/js/main.js
@@ -1,6 +1,33 @@
 (function (window) {
     'use strict';
+    // 暂时封闭，写个测试
+    // var App = window.App || {};
+    // var Truck = App.Truck;
+    // var DataStore = App.DataStore;
+    // truckID = 'ncc-1701';
+    // var myTruck = new Truck(truckID, new DataStore());
+    // window.myTruck = myTruck;
+
     var App = window.App || {};
-    var Truck = App.Truck;
-    var DataStore = App.DataStore;
+    var launchCount = 0;
+
+    function Spaceship(shipname) {
+        var Truck = App.Truck;
+        var DataStore = App.DataStore;
+        var myTruck = new Truck(shipname, new DataStore());
+        window.myTruck = myTruck;
+    }
+
+    Spaceship.prototype.blastoff = function () {
+        // 闭包允许访问 launchCount 属性
+        launchCount++;
+        console.log("Spaceship Launched!");
+    }
+
+    Spaceship.prototype.reportLaunchCount = function () {
+        console.log("Total number of launches: " + launchCount);
+    }
+
+    App.Spaceship = Spaceship;
+    window.App = App;
 })(window);
