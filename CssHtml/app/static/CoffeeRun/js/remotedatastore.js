8298a41147bc1a1822483ce5cde55df1d0d6cfd3
orinbai <orinbai@sina.com>
2021-07-09 23:11:33 +0800

    remote

---
+++ CssHtml/app/static/CoffeeRun/js/remotedatastore.js
@@ -0,0 +1,31 @@
+(function (window) {
+    'use strict';
+    var App = window.App || {};
+    var $ = window.jQuery;
+
+    function RemoteDataStore(url) {
+        if (!url) {
+            throw new Error('No remote URL supplied.');
+        }
+
+        this.serverUrl = url;
+    }
+
+    RemoteDataStore.prototype.add = function (key, val) {
+        // coding
+        $.post(this.serverUrl, val, function (serverResponse) {
+            console.log(serverResponse);
+        });
+    };
+
+    RemoteDataStore.prototype.getAll = function (cb) {
+        //coding
+        $.get(this.serverUrl, function (serverResponse) {
+            console.log(serverResponse);
+            cb(serverResponse);
+        });
+    };
+
+    App.RemoteDataStore = RemoteDataStore;
+    window.App = App;
+})(window);
