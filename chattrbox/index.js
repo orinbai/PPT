41e1323687d74651c47cc6fe1c73cd139a83424d
orinbai <orinbai@sina.com>
2021-07-23 22:48:35 +0800

    chattrbox

---
+++ chattrbox/index.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+var server = http.createServer(function (req, res) {
+    console.log('Responding to a request.');
+    var url = req.url;
+
+    // res.end('<h1>Hello, World!</h1>');
+    var fileName = 'index.html';
+    if (url.length > 1) {
+        fileName = url.substring(1)
+    }
+    console.log(fileName);
+    var filePath = path.resolve(__dirname, 'app', fileName);
+    // fs.readFile('app/index.html', function (err, data) {
+    fs.readFile(filePath, function (err, data) {
+        res.end(data);
+    })
+});
+server.listen(3666)
