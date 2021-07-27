d3c92c25e52e5d24127937f83583b37e6d5f16fa
orinbai <orinbai@sina.com>
2021-07-27 22:26:20 +0800

    node.js 学习

--- chattrbox/index.js
+++ chattrbox/index.js
@@ -1,21 +1,36 @@
 var http = require('http');
 var fs = require('fs');
-var path = require('path');
+// var path = require('path');
+var extract = require('./extract');
+// var extractFilePath = require('./extract');
+var wss = require('./websockets-server');
+
+var handleError = function (err, res) {
+    res.writeHead(404);
+    res.end();
+};
 
 var server = http.createServer(function (req, res) {
     console.log('Responding to a request.');
-    var url = req.url;
+    // var url = req.url;
 
-    // res.end('<h1>Hello, World!</h1>');
-    var fileName = 'index.html';
-    if (url.length > 1) {
-        fileName = url.substring(1)
-    }
-    console.log(fileName);
-    var filePath = path.resolve(__dirname, 'app', fileName);
+    // // res.end('<h1>Hello, World!</h1>');
+    // var fileName = 'index.html';
+    // if (url.length > 1) {
+    //     fileName = url.substring(1)
+    // }
+    // console.log(fileName);
+    // var filePath = path.resolve(__dirname, 'app', fileName);
+    var filePath = extract(req.url);
     // fs.readFile('app/index.html', function (err, data) {
     fs.readFile(filePath, function (err, data) {
-        res.end(data);
+        if (err) {
+            handleError(err, res);
+            return;
+        } else {
+            res.end(data);
+        }
+        // res.end(data);zai
     })
 });
 server.listen(3666)
